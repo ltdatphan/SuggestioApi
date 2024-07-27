b65cf02e3ab17166e722b26c7fa790e701b23312
David Phan <29266892+ltdatphan@users.noreply.github.com>
2024-07-27 18:08:09 -0400

    update persistence config

--- DependencyInjection/PersistenceExtensions.cs
+++ DependencyInjection/PersistenceExtensions.cs
@@ -1,40 +1,66 @@
+using System.Web;
 using Microsoft.EntityFrameworkCore;
+using Npgsql;
 using SuggestioApi.Data;
 
 namespace SuggestioApi.DependencyInjection;
 
 public static class PersistenceExtensions
 {
-    public static IServiceCollection AddPersistence(this IServiceCollection services, IConfiguration configuration, IWebHostEnvironment environment)
+    public static IServiceCollection AddPersistence(this IServiceCollection services, IConfiguration configuration,
+        IWebHostEnvironment environment)
     {
-        string connectionString = String.Empty;
-        
+        var connectionString = string.Empty;
+
         if (environment.IsDevelopment())
+        {
             connectionString = configuration.GetConnectionString("Sql");
+        }
         else
         {
-            // Use connection string provided at runtime by Fly.
-            var connUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
-    
-            // Parse connection URL to connection string for Npgsql
-            connUrl = connUrl.Replace("postgres://", string.Empty);
-            var pgUserPass = connUrl.Split("@")[0];
-            var pgHostPortDb = connUrl.Split("@")[1];
-            var pgHostPort = pgHostPortDb.Split("/")[0];
-            var pgDb = pgHostPortDb.Split("/")[1];
-            var pgUser = pgUserPass.Split(":")[0];
-            var pgPass = pgUserPass.Split(":")[1];
-            var pgHost = pgHostPort.Split(":")[0];
-            var pgPort = pgHostPort.Split(":")[1];
-            var updatedHost = pgHost.Replace("flycast", "internal");
+            // // Use connection string provided at runtime by Fly.
+            // var connUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+            //
+            // // Parse connection URL to connection string for Npgsql
+            // connUrl = connUrl.Replace("postgres://", string.Empty);
+            // var pgUserPass = connUrl.Split("@")[0];
+            // var pgHostPortDb = connUrl.Split("@")[1];
+            // var pgHostPort = pgHostPortDb.Split("/")[0];
+            // var pgDb = pgHostPortDb.Split("/")[1];
+            // var pgUser = pgUserPass.Split(":")[0];
+            // var pgPass = pgUserPass.Split(":")[1];
+            // var pgHost = pgHostPort.Split(":")[0];
+            // var pgPort = pgHostPort.Split(":")[1];
+            // var updatedHost = pgHost.Replace("flycast", "internal");
 
-            connectionString = $"Server={updatedHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb};";
-        };
-        
-        services.AddDbContext<ApplicationDBContext>(options =>
-        {
-            options.UseNpgsql(connectionString);
-        });
+            var databaseUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+            if (!string.IsNullOrEmpty(databaseUrl))
+            {
+                var dbUri = new Uri(databaseUrl);
+                var userInfo = dbUri.UserInfo.Split(':');
+                var query = HttpUtility.ParseQueryString(dbUri.Query);
+                var sslMode = query.Get("sslmode") ?? "Require";
+
+                var connectionStringBuilder = new NpgsqlConnectionStringBuilder
+                {
+                    Host = dbUri.Host,
+                    Port = dbUri.Port,
+                    Username = userInfo[0],
+                    Password = userInfo[1],
+                    Database = dbUri.LocalPath.TrimStart('/'),
+                    SslMode = sslMode.Equals("disable", StringComparison.OrdinalIgnoreCase)
+                        ? SslMode.Disable
+                        : SslMode.Require,
+                    TrustServerCertificate = sslMode.Equals("disable", StringComparison.OrdinalIgnoreCase)
+                };
+
+                connectionString = connectionStringBuilder.ToString();
+            }
+        }
+
+        ;
+
+        services.AddDbContext<ApplicationDBContext>(options => { options.UseNpgsql(connectionString); });
 
         return services;
     }
